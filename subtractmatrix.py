418cc39653ebd88fe81cbda54bccf8085a00e299
Anjana-sankar-k <151863605+Anjana-sankar-k@users.noreply.github.com>
2024-12-18 14:48:29 +0530

    Add files via upload

---
+++ subtractmatrix.py
@@ -0,0 +1,14 @@
+def subtract_matrices(mat1, mat2):
+  rows, cols = len(mat1), len(mat1[0])
+  result = [[mat1[i][j] - mat2[i][j] for j in range(cols)] for i in range(rows)]
+  return result
+  
+  
+matrix1 = [[1, 2, 3], [4, 5, 6], [ 7, 8, 9]]
+matrix2 = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+
+result = subtract_matrices(matrix1, matrix2)
+print("Resultant matrix: ")
+for row in result: 
+  print(row)
+  
