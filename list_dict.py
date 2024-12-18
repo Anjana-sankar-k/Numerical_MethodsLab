44048ecceaec79818eafd06b81a4d177382beac4
Anjana-sankar-k <151863605+Anjana-sankar-k@users.noreply.github.com>
2024-12-18 14:54:45 +0530

    first lab hour

---
+++ list_dict.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 4, 5]
+print("Original list: ", my_list)
+my_list.append(6)
+my_list.remove(1)
+
+print("Updated list: ", my_list)
+
+my_dict = {"anjana" : 1, "mingyu" : 2, "tom" : 3}
+print("Original dictionary: ", my_dict)
+my_dict["zendaya"] = 4
+
+del my_dict["tom"]
+print("Updated dictionary: ", my_dict)
